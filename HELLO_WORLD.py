fd7ec7565a5edcb5d2f724482531aa8f4e89f272
Muhammad Shakil Tariq <mshakeelt@users.noreply.github.com>
2021-01-24 19:02:48 +0100

    First Commit

---
+++ HELLO_WORLD.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+
+print(tf.__version__)
+
+a = tf.constant([2], name = 'constant_a')
+b = tf.constant([3], name = 'constant_b')
+print(a)
+tf.print(a.numpy()[0])
+
+@tf.function
+def add(a,b):
+    c = tf.add(a, b)
+    #c = a + b is also a way to define the sum of the terms
+    print(c)
+    return c
+
+result = add(a,b)
+tf.print(result[0])
+
+Scalar = tf.constant(2)
+Vector = tf.constant([5,6,2])
+Matrix = tf.constant([[1,2,3],[2,3,4],[3,4,5]])
+Tensor = tf.constant( [ [[1,2,3],[2,3,4],[3,4,5]] , [[4,5,6],[5,6,7],[6,7,8]] , [[7,8,9],[8,9,10],[9,10,11]] ] )
+
+print ("Scalar (1 entry):\n %s \n" % Scalar)
+
+print ("Vector (3 entries) :\n %s \n" % Vector)
+
+print ("Matrix (3x3 entries):\n %s \n" % Matrix)
+
+print ("Tensor (3x3x3 entries) :\n %s \n" % Tensor)
+
+print(Scalar.shape)
+Tensor.shape
+
