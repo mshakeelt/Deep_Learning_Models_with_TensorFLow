b7fee97ab20f25b208853df70a0fbb35c5559090
Muhammad Shakil Tariq <mshakeelt@users.noreply.github.com>
2021-01-24 19:14:38 +0100

    Final Update in the First Exercise

--- HELLO_WORLD.py
+++ HELLO_WORLD.py
@@ -34,3 +34,55 @@ print ("Tensor (3x3x3 entries) :\n %s \n" % Tensor)
 print(Scalar.shape)
 Tensor.shape
 
+Matrix_one = tf.constant([[1,2,3],[2,3,4],[3,4,5]])
+Matrix_two = tf.constant([[2,2,2],[2,2,2],[2,2,2]])
+
+@tf.function
+def add():
+    add_1_operation = tf.add(Matrix_one, Matrix_two)
+    return add_1_operation
+
+
+
+print ("Defined using tensorflow function :")
+add_1_operation = add()
+print(add_1_operation)
+print ("Defined using normal expressions :")
+add_2_operation = Matrix_one + Matrix_two
+print(add_2_operation)
+
+Matrix_one = tf.constant([[2,3],[3,4]])
+Matrix_two = tf.constant([[2,3],[3,4]])
+
+@tf.function
+def mathmul():
+  return tf.matmul(Matrix_one, Matrix_two)
+
+
+mul_operation = mathmul()
+
+print ("Defined using tensorflow function :")
+print(mul_operation)
+
+v = tf.Variable(0)
+
+@tf.function
+def increment_by_one(v):
+        v = tf.add(v,1)
+        return v
+for i in range(3):
+    v = increment_by_one(v)
+    print(v)
+
+a = tf.constant([5])
+b = tf.constant([2])
+c = tf.add(a,b)
+d = tf.subtract(a,b)
+
+
+print ('c =: %s' % c)
+    
+print ('d =: %s' % d)
+
+
+
