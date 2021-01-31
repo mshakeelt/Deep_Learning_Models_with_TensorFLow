9900b71782fa8fb96481d705605b7cfc88614e3d
Muhammad Shakil Tariq <mshakeelt@users.noreply.github.com>
2021-01-31 23:34:48 +0100

    Small LSTM

--- Long_Short_Term_Memory_Model(LSTM).py
+++ Long_Short_Term_Memory_Model(LSTM).py
@@ -11,3 +11,22 @@ lstm = tf.keras.layers.LSTM(LSTM_CELL_SIZE, return_sequences=True, return_state=
 lstm.states=state
 
 print(lstm.states)
+
+#Batch size x time steps x features.
+sample_input = tf.constant([[3,2,2,2,2,2]],dtype=tf.float32)
+
+batch_size = 1
+sentence_max_length = 1
+n_features = 6
+
+new_shape = (batch_size, sentence_max_length, n_features)
+
+inputs = tf.constant(np.reshape(sample_input, new_shape), dtype = tf.float32)
+
+output, final_memory_state, final_carry_state = lstm(inputs)
+
+print('Output : ', tf.shape(output))
+
+print('Memory : ',tf.shape(final_memory_state))
+
+print('Carry state : ',tf.shape(final_carry_state))
