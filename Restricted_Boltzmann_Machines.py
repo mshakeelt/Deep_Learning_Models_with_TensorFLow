bc033f87b5a669e9bcc1350e416639c15ee712c6
Muhammad Shakil Tariq <mshakeelt@users.noreply.github.com>
2021-02-09 00:05:27 +0100

    Sample application of RBM

--- Restricted_Boltzmann_Machines.py
+++ Restricted_Boltzmann_Machines.py
@@ -17,3 +17,29 @@ h_bias = tf.Variable(tf.zeros([2]), tf.float32)     # Bias unit for hidden layer
 
 W = tf.constant(np.random.normal(loc=0.0, scale=1.0, size=(7, 2)).astype(np.float32))   #Weights between visible and hidden layers
 
+#>>>>>>>>>>>>>>>Example<<<<<<<<<<<<<<<<<<<<<<<<<<<
+X = tf.constant([[1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]], tf.float32)
+
+v_state = X
+print ("Input: ", v_state)
+
+h_bias = tf.constant([0.1, 0.1])
+print ("hb: ", h_bias)
+print ("w: ", W)
+
+# Calculate the probabilities of turning the hidden units on:
+h_prob = tf.nn.sigmoid(tf.matmul(v_state, W) + h_bias)  #probabilities of the hidden units
+print ("p(h|v): ", h_prob)
+
+# Draw samples from the distribution:
+h_state = tf.nn.relu(tf.sign(h_prob - tf.random.uniform(tf.shape(h_prob)))) #states
+print ("h0 states:", h_state)
+
+#Reconstruction
+vb = tf.constant([0.1, 0.2, 0.1, 0.1, 0.1, 0.2, 0.1])
+print ("b: ", vb)
+v_prob = tf.nn.sigmoid(tf.matmul(h_state, tf.transpose(W)) + vb)
+print ("p(vi|h): ", v_prob)
+v_state = tf.nn.relu(tf.sign(v_prob - tf.random.uniform(tf.shape(v_prob))))
+print ("v probability states: ", v_state)
+
