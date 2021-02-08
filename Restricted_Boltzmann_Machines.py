2ecd3dc7e3a836a2f17efbef70befb2452b3e417
Muhammad Shakil Tariq <mshakeelt@users.noreply.github.com>
2021-02-09 00:22:08 +0100

    network structure for MNIST dataset

--- Restricted_Boltzmann_Machines.py
+++ Restricted_Boltzmann_Machines.py
@@ -43,3 +43,55 @@ print ("p(vi|h): ", v_prob)
 v_state = tf.nn.relu(tf.sign(v_prob - tf.random.uniform(tf.shape(v_prob))))
 print ("v probability states: ", v_state)
 
+#>>>>>>>>>>>>Loading MNIST data for RBM Application<<<<<<<<<<<<<<<<<
+#loading training and test data
+mnist = tf.keras.datasets.mnist
+(trX, trY), (teX, teY) = mnist.load_data()
+
+# showing an example of the Flatten class and operation
+from tensorflow.keras.layers import Flatten
+flatten = Flatten(dtype='float32')
+trX = flatten(trX/255.0)
+trY = flatten(trY/255.0)
+
+#>>>>>>>>>>>>RBM Network for MNIST data<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+vb = tf.Variable(tf.zeros([784]), tf.float32)   #784 neurons in the input layer (784 pixels)
+hb = tf.Variable(tf.zeros([50]), tf.float32)    #50 hidden neurons
+W = tf.Variable(tf.zeros([784,50]), tf.float32)
+
+v0_state = tf.Variable(tf.zeros([784]), tf.float32)
+
+#testing to see if the matrix product works
+tf.matmul( [v0_state], W)
+
+#computing the hidden nodes probability vector and checking shape
+h0_prob = tf.nn.sigmoid(tf.matmul([v0_state], W) + hb)  #probabilities of the hidden units
+print("h0_state shape: " , tf.shape(h0_prob))
+
+#defining a function to return only the generated hidden states 
+def hidden_layer(v0_state, W, hb):
+    h0_prob = tf.nn.sigmoid(tf.matmul([v0_state], W) + hb)  #probabilities of the hidden units
+    h0_state = tf.nn.relu(tf.sign(h0_prob - tf.random.uniform(tf.shape(h0_prob)))) #sample_h_given_X
+    return h0_state
+
+
+h0_state = hidden_layer(v0_state, W, hb)
+print("first 15 hidden states: ", h0_state[0][0:15])
+
+#Reconstruction
+def reconstructed_output(h0_state, W, vb):
+    v1_prob = tf.nn.sigmoid(tf.matmul(h0_state, tf.transpose(W)) + vb) 
+    v1_state = tf.nn.relu(tf.sign(v1_prob - tf.random.uniform(tf.shape(v1_prob)))) #sample_v_given_h
+    return v1_state[0]
+
+v1_state = reconstructed_output(h0_state, W, vb)
+print("hidden state shape: ", h0_state.shape)
+print("v0 state shape:  ", v0_state.shape)
+print("v1 state shape:  ", v1_state.shape)
+
+#Cost Function
+def error(v0_state, v1_state):
+    return tf.reduce_mean(tf.square(v0_state - v1_state))
+
+err = tf.reduce_mean(tf.square(v0_state - v1_state))
+print("error" , err.numpy())
