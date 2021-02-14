fd575f3088d6053b4e963077607cf3b1375fae0e
muta1723 <muhammad-shakil.tariq@tu-ilmenau.de>
2021-02-15 00:29:46 +0100

    building encoder and decoder

--- autoencoders.py
+++ autoencoders.py
@@ -40,3 +40,34 @@ n_hidden_2 = 128 # 2nd layer num features
 encoding_layer = 32 # final encoding bottleneck features
 n_input = 784 # MNIST data input (img shape: 28*28)
 
+#>>>>>>>>>>>>>>Encoder<<<<<<<<<<<<<<<
+
+enocoding_1 = tf.keras.layers.Dense(n_hidden_1, activation=tf.nn.sigmoid)
+encoding_2 = tf.keras.layers.Dense(n_hidden_2, activation=tf.nn.sigmoid)
+encoding_final = tf.keras.layers.Dense(encoding_layer, activation=tf.nn.relu)
+
+# Building the encoder
+def encoder(x):
+    x_reshaped = flatten_layer(x)
+    # Encoder first layer with sigmoid activation #1
+    layer_1 = enocoding_1(x_reshaped)
+    # Encoder second layer with sigmoid activation #2
+    layer_2 = encoding_2(layer_1)
+    code = encoding_final(layer_2)
+    return code
+
+#>>>>>>>>>>>>>>Decoder<<<<<<<<<<<<<<<<<<
+
+decoding_1 = tf.keras.layers.Dense(n_hidden_2, activation=tf.nn.sigmoid)
+decoding_2 = tf.keras.layers.Dense(n_hidden_1, activation=tf.nn.sigmoid)
+decoding_final = tf.keras.layers.Dense(n_input)
+# Building the decoder
+def decoder(x):
+    # Decoder first layer with sigmoid activation #1
+    layer_1 = decoding_1(x)
+    # Decoder second layer with sigmoid activation #2
+    layer_2 = decoding_2(layer_1)
+    decode = self.decoding_final(layer_2)
+    return decode
+
+    
